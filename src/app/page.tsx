d67c81c4a6407cde962b6a7b0d9717a1e1f7d025
utkarsh <khareu550@gmail.com>
2024-04-26 03:06:07 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Home from "@/components/Home";
+export default function HomePage() {
+  return (
+   <>
+    <Home />
+   </> 
+  );
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,75 @@
+"use client";
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import logo from '@/asset/logo.png'
+import { Button } from '@/components/ui/button'
+import { LuPhoneCall } from "react-icons/lu";
+import { RiMenu3Line } from "react-icons/ri";
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover"
+function Header() {
+    return (
+        <>
+            <>
+                <div className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/50 ">
+                    <header className="flex h-16 w-full shrink-0 items-center justify-between px-2 md:px-8">
+                        <Link className="mr-6" href="/">
+                            <Image src={logo} alt="logo" width={110} />
+                            <span className="sr-only">Bajrang Enterprises</span>
+                        </Link>
+
+                        <nav className="flex items-center space-x-6 hidden md:block">
+                            <Link className='hover:text-primary font-semibold' href="/about">Products</Link>
+                            <Link className='hover:text-primary font-semibold' href="/contact">Reviews</Link>
+                            <Link className='hover:text-primary font-semibold' href="/login">Service</Link>
+                            <Link className='hover:text-primary font-semibold' href="/signup">Contact Us</Link>
+                        </nav>
+
+                        <div className='flex items-center gap-2'>
+                            <div>
+                                <Button>
+                                    <LuPhoneCall className="mr-2" />
+                                    <span>Inquire Now</span>
+                                </Button>
+                            </div>
+
+                            <div className='block md:hidden'>
+
+
+                                <Popover>
+                                    <PopoverTrigger asChild>
+                                        <Button variant={'ghost'} className='text-3xl hover:text-primary'>
+                                            <RiMenu3Line />
+                                        </Button>
+                                    </PopoverTrigger>
+                                    <PopoverContent className="w-dvw mt-2">
+                                        <div className="grid gap-4">
+                                            <div className="space-y-2">
+                                                <nav className='flex flex-col gap-3'>
+                                                    <Link className='hover:text-primary font-semibold' href="/about">Products</Link>
+                                                    <Link className='hover:text-primary font-semibold' href="/contact">Reviews</Link>
+                                                    <Link className='hover:text-primary font-semibold' href="/login">Service</Link>
+                                                    <Link className='hover:text-primary font-semibold' href="/signup">Contact Us</Link>
+                                                </nav>
+                                            </div>
+
+                                        </div>
+                                    </PopoverContent>
+                                </Popover>
+                            </div>
+                        </div>
+
+
+
+                    </header>
+                </div>
+            </>
+        </>
+    )
+}
+
+export default Header
